e891e129d949ea10a3861d61de702032768cd510
Bas van der Hoek <bvdhoek@zilverline.com>
2018-02-02 14:23:51 +0100

    simple test of mongodb with mongoose and chai

---
+++ README.md
@@ -0,0 +1,11 @@
+    npm install -g mocha
+    npm init; npm install mongoose chai
+    
+    mocha
+      Database Tests
+        Test Database
+          ✓ New name saved to test database (47ms)
+          ✓ Dont save incorrect format to database
+          ✓ Should retrieve data from test database
+    
+      3 passing (108ms)
---
+++ test/mongoose.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const testSchema = new Schema({
+  name: {type: String, required: true}
+});
+const Name = mongoose.model('Name', testSchema);
+
+describe('Database Tests', () =>{
+  before(function (done) {
+    mongoose.connect('mongodb://localhost/testDatabase');
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error'));
+    db.once('open', done);
+  });
+
+  describe('Test Database', () =>{
+    it('New name saved to test database', done =>{
+      const testName = Name({name: 'Mike'});
+
+      testName.save(done);
+    });
+
+    it('Dont save incorrect format to database', done =>{
+      const wrongSave = Name({
+        notName: 'Not Mike'
+      });
+      wrongSave.save(err => {
+        if (err) {
+          return done();
+        }
+        throw new Error('Should generate error!');
+      });
+    });
+
+    it('Should retrieve data from test database', done =>{
+      Name.find({name: 'Mike'}, (err, name) => {
+        if (err) {
+          throw err;
+        }
+        if (name.length === 0) {
+          throw new Error('No data!');
+        }
+        done();
+      });
+    });
+  });
+
+  after(function (done) {
+    mongoose.connection.db.dropDatabase(function () {
+      mongoose.connection.close(done);
+    });
+  });
+});
